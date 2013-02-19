f050645ab9069dda9ce410a29a84120f6a4028bf
Victor Varvaryuk <v.varvariuc@corp.mail.ru>
2013-02-19 16:24:54 +0400

    .

--- spreadsheet.js
+++ spreadsheet.js
@@ -21,328 +21,11 @@ var sys = {
 var table;
 
 $(function() {
-	table = document.getElementById('table');
+	table = document.getElementById('table')
 	// cursor keys only in keydown
 	// $(document).on("keydown", keypress);
 	// $(".ui-dialog-content").css("padding", 0);
 	// disable selection on the page
-	$(document).on("selectstart", false);
-
-	$(document).on("dblclick", "#table > tbody > tr > td.cell", function(e) {
-		if (e.which != 1 || e.shiftKey || e.altKey || e.ctrlKey)
-			// need left button without keyboard modifiers
-			return;
-		formatSelection();
-	});
-
-	$(document).on("mousedown", "#table > tbody > tr > td", function(e) {
-		if (e.which != 1 || e.shiftKey || e.altKey || e.ctrlKey)
-			// need left button without keyboard modifiers
-			return;
-		reset_selection();
-		sys.isMouseDown = true;
-		sys.selectionStart = this;
-		on_mouse_over_cell(this);
-	});
-
-	$(document).on("mouseup", "#table > tbody > tr > td", function(e) {
-		if (!sys.isMouseDown)
-			return;
-		sys.isMouseDown = false;
-	});
-
-	$(document).on("mouseover", "#table > tbody > tr > td", function(e) {
-		// $("#status_bar").text('' + this.parentNode.rowIndex + ', ' + this.cellIndex);
-		on_mouse_over_cell(this);
-	});
-
-	function on_mouse_over_cell(selectionEnd) {
-		if (sys.isMouseDown && sys.selectionEnd !== selectionEnd)
-			set_selection(sys.selectionStart, selectionEnd);
-	}
-
-	function set_selection(selectionStart, selectionEnd) {
-
-		function select(r1, c1, r2, c2) {
-			sys.selectionHasMergedCells = false;
-			sys.r1 = r1;
-			sys.c1 = c1;
-			sys.r2 = r2;
-			sys.c2 = c2;
-			// not very nice iterative solution, but it's simple and it works. A recursive solution would be nicer.
-			for (var r = r1; r <= r2; r++) {
-				for (var c = c1; c <= c2; c++) {
-					var cell = table.rows[r].cells[c];
-					var _r2 = Math.max(r2, r + cell.rowSpan - 1)
-					var _c2 = Math.max(c2, c + cell.colSpan - 1)
-					if (_r2 > r2 || _c2 > c2) {
-						select(r1, c1, _r2, _c2);
-						return;
-					}
-					if (cell.classList.contains('merged_cell')) {
-						sys.selectionHasMergedCells = true;
-						var _ = cell.getAttribute('data-merged').split(',');
-						var _r1 = Math.min(parseInt(_[0]), r1);
-						var _c1 = Math.min(parseInt(_[1]), c1);
-						if (_r1 < r1 || _c1 < c1) {
-							select(_r1, _c1, r2, c2);
-							return;
-						}
-					}
-					cell.classList.add('cell_selected');
-				}
-			}
-		}
-
-		clear_selection();
-		sys.selectionStart = selectionStart;
-		sys.selectionEnd = selectionEnd;
-
-		if (table.rows.length == 2) {
-			sys.r1 = null;
-			sys.r2 = null;
-			sys.c1 = selectionStart.cellIndex;
-			sys.c2 = selectionEnd.cellIndex;
-			return
-		} else if (table.rows[0].cells.length == 2) {
-			sys.r1 = selectionStart.parentNode.rowIndex;
-			sys.c1 = null;
-			sys.r2 = selectionEnd.parentNode.rowIndex;
-			sys.c2 = null;
-			return
-		}
-		var _ = normalize_selection(selectionStart, selectionEnd);
-		select(Math.max(_.r1, 2), Math.max(_.c1, 2), _.r2, _.c2);
-
-	}
-
-	function clear_selection() {
-		if (sys.r1 !== null && sys.c1 !== null)
-			for (var rowNo = sys.r1; rowNo <= sys.r2; rowNo++) {
-				for (var colNo = sys.c1; colNo <= sys.c2; colNo++)
-					table.rows[rowNo].cells[colNo].classList.remove('cell_selected');
-			}
-		sys.r1 = null;
-		sys.c1 = null;
-		sys.r2 = null;
-		sys.c2 = null;
-	}
-
-	function reset_selection() {
-		clear_selection();
-		sys.selectionStart = sys.selectionEnd = null;
-	}
-
-	function normalize_selection(selectionStart, selectionEnd) {
-		var r1, c1, r2, c2;
-		if (selectionStart.classList.contains('cell')) {
-			// a simple cell was the start
-			r1 = selectionStart.parentNode.rowIndex;
-			c1 = selectionStart.cellIndex;
-			r2 = selectionEnd.parentNode.rowIndex;
-			c2 = selectionEnd.cellIndex;
-		} else if (selectionStart.classList.contains('colheader')) {
-			// a column header was the start
-			r1 = 2;
-			c1 = selectionStart.cellIndex;
-			r2 = table.rows.length - 1;
-			c2 = selectionEnd.cellIndex;
-		} else if (selectionStart.classList.contains('rowheader')) {
-			// a row header was the start
-			r1 = selectionStart.parentNode.rowIndex;
-			c1 = 2;
-			r2 = selectionEnd.parentNode.rowIndex;
-			c2 = table.rows[0].cells.length - 1;
-		}
-		return {
-			r1 : Math.min(r1, r2),
-			c1 : Math.min(c1, c2),
-			r2 : Math.max(r1, r2),
-			c2 : Math.max(c1, c2),
-		}
-	}
-
-	function mergeSelectedCells() {
-		for (var r = sys.r1; r <= sys.r2; r++) {
-			for (var c = sys.c1; c <= sys.c2; c++) {
-				if (r == sys.r1 && c == sys.c1)
-					continue;
-				var cell = table.rows[r].cells[c];
-				cell.classList.add('merged_cell');
-				cell.setAttribute('data-merged', [sys.r1, sys.c1].join(','));
-			}
-		}
-		var cell = table.rows[sys.r1].cells[sys.c1];
-		cell.colSpan = sys.c2 - sys.c1 + 1;
-		cell.rowSpan = sys.r2 - sys.r1 + 1;
-		sys.selectionHasMergedCells = true;
-	}
-
-	function splitSelectedCells() {
-		for (var r = sys.r1; r <= sys.r2; r++) {
-			for (var c = sys.c1; c <= sys.c2; c++) {
-				var cell = table.rows[r].cells[c];
-				cell.classList.remove('merged_cell');
-				cell.removeAttribute('data-merged');
-				cell.rowSpan = 1;
-				cell.colSpan = 1;
-			}
-		}
-		sys.selectionHasMergedCells = false;
-	}
-
-	function clearSelectionContents() {
-		for (var r = sys.r1; r <= sys.r2; r++) {
-			for (var c = sys.c1; c <= sys.c2; c++) {
-				var cell = table.rows[r].cells[c];
-				cell.innerHTML = '';
-			}
-		}
-	}
-
-
-	$.contextMenu({
-		selector : '#table > tbody > tr > td.cell_selected',
-		build : function($trigger, e) {
-			var items = {};
-			if (sys.selectionHasMergedCells)
-				items['split_cells'] = {
-					'name' : "Split merged cells",
-					'callback' : splitSelectedCells,
-					'icon' : 'split_cell',
-				};
-			else if (sys.selectionStart !== sys.selectionEnd)
-				items['mergeSelectedCells'] = {
-					'name' : "Merge selected cells",
-					'callback' : mergeSelectedCells,
-					'icon' : 'merge_cells',
-				};
-			items['clear_contents'] = {
-				'name' : "Clear contents",
-				'callback' : clearSelectionContents,
-				'icon' : 'clear_cell_contents',
-			};
-			items['formatSelectedCells'] = {
-				'name' : "Format...",
-				'callback' : formatSelection,
-			};
-
-			return {
-				'items' : items
-			}
-		}
-	});
-
-	$.contextMenu({
-		selector : '#table > tbody > tr > td.colheader',
-		build : function($trigger, e) {
-			var el = $trigger[0];
-			if (!sys.selectionStart)
-				set_selection(el, el);
-			else {
-				if (sys.selectionStart.classList.contains('colheader')) {
-					if (el.cellIndex < sys.c1 || el.cellIndex > sys.c2)
-						set_selection(el, el);
-				} else
-					set_selection(el, el);
-			}
-			return {
-				items : {
-					insert_columns : {
-						name : "Insert columns",
-						callback : function(e) {
-							addColumns(sys.c2 - sys.c1 + 1, sys.c1 - 2);
-							set_selection(sys.selectionStart, sys.selectionEnd);
-						}
-					},
-					remove_columns : {
-						name : "Remove columns",
-						callback : function(e) {
-							removeColumns(sys.c1 - 2, sys.c2 - sys.c1 + 1);
-							sys.r1 = null;
-							reset_selection();
-						}
-					},
-				}
-			}
-		}
-	});
-
-	$.contextMenu({
-		selector : '#table > tbody > tr > td.rowheader',
-		build : function($trigger, e) {
-			var el = $trigger[0];
-			if (!sys.selectionStart)
-				set_selection(el, el);
-			else {
-				if (sys.selectionStart.classList.contains('rowheader')) {
-					if (el.parentNode.rowIndex < sys.r1 || el.parentNode.rowIndex > sys.r2)
-						set_selection(el, el);
-				} else
-					set_selection(el, el);
-			}
-			return {
-				items : {
-					insert_rows : {
-						name : "Insert rows",
-						callback : function(e) {
-							addRows(sys.r2 - sys.r1 + 1, sys.r1 - 2);
-							set_selection(sys.selectionStart, sys.selectionEnd);
-						}
-					},
-				}
-			}
-		}
-	});
-
-	$.contextMenu({
-		selector : '#table > tbody > tr > td.corner',
-		build : function($trigger, e) {
-			var el = $trigger[0];
-			return {
-				items : {
-					add_column : {
-						name : "Add a column",
-						callback : function(e) {
-							addColumns();
-						}
-					},
-					add_row : {
-						name : "Add a row",
-						callback : function(e) {
-							addRows();
-						}
-					},
-				}
-			}
-		}
-	});
-
-	$(document).on("mousedown", "#table > tbody > tr > td.colheader, #table > tbody > tr > td.rowheader", function(e) {
-		if (!e.shiftKey)
-			return;
-		sys.draggingStart = $(this);
-		sys.draggingStart.data("startX", e.pageX);
-		sys.draggingStart.data("startY", e.pageY);
-		sys.draggingStart.data("startWidth", $(this).width());
-		sys.draggingStart.data("startHeight", $(this).height());
-	});
-
-	$(document).on("mousemove", function(e) {
-		if (sys.draggingStart) {
-			if (sys.draggingStart.hasClass("colheader"))
-				sys.draggingStart.width(sys.draggingStart.data("startWidth") + (e.pageX - sys.draggingStart.data("startX")));
-			else
-				sys.draggingStart.height(sys.draggingStart.data("startHeight") + (e.pageY - sys.draggingStart.data("startY")));
-		}
-	});
-
-	$(document).on("mouseup", function() {
-		if (sys.draggingStart) {
-			sys.draggingStart = null;
-		}
-	});
-
 	addColumns(10);
 	addRows(10);
 	for (var r = 2; r < table.rows.length; r++) {
@@ -353,6 +36,234 @@ $(function() {
 		}
 	}
 
+});
+
+$(document).on("selectstart", false);
+
+$(document).on("dblclick", "#table > tbody > tr > td.cell", function(e) {
+	if (e.which != 1 || e.shiftKey || e.altKey || e.ctrlKey)
+		// need left button without keyboard modifiers
+		return;
+	formatSelection();
+});
+
+$(document).on("mousedown", "#table > tbody > tr > td", function(e) {
+	if (e.which != 1 || e.shiftKey || e.altKey || e.ctrlKey)
+		// need left button without keyboard modifiers
+		return;
+	reset_selection();
+	sys.isMouseDown = true;
+	sys.selectionStart = this;
+	on_mouse_over_cell(this);
+});
+
+$(document).on("mouseup", "#table > tbody > tr > td", function(e) {
+	if (!sys.isMouseDown)
+		return;
+	sys.isMouseDown = false;
+});
+
+$(document).on("mouseover", "#table > tbody > tr > td", function(e) {
+	// $("#status_bar").text('' + this.parentNode.rowIndex + ', ' + this.cellIndex);
+	on_mouse_over_cell(this);
+});
+
+function on_mouse_over_cell(selectionEnd) {
+	if (sys.isMouseDown && sys.selectionEnd !== selectionEnd)
+		set_selection(sys.selectionStart, selectionEnd);
+}
+
+function set_selection(selectionStart, selectionEnd) {
+
+	function select(r1, c1, r2, c2) {
+		sys.selectionHasMergedCells = false;
+		sys.r1 = r1;
+		sys.c1 = c1;
+		sys.r2 = r2;
+		sys.c2 = c2;
+		// not very nice iterative solution, but it's simple and it works. A recursive solution would be nicer.
+		for (var r = r1; r <= r2; r++) {
+			for (var c = c1; c <= c2; c++) {
+				var cell = table.rows[r].cells[c];
+				var _r2 = Math.max(r2, r + cell.rowSpan - 1)
+				var _c2 = Math.max(c2, c + cell.colSpan - 1)
+				if (_r2 > r2 || _c2 > c2) {
+					select(r1, c1, _r2, _c2);
+					return;
+				}
+				if (cell.classList.contains('merged_cell')) {
+					sys.selectionHasMergedCells = true;
+					var _ = cell.getAttribute('data-merged').split(',');
+					var _r1 = Math.min(parseInt(_[0]), r1);
+					var _c1 = Math.min(parseInt(_[1]), c1);
+					if (_r1 < r1 || _c1 < c1) {
+						select(_r1, _c1, r2, c2);
+						return;
+					}
+				}
+				cell.classList.add('cell_selected');
+			}
+		}
+	}
+
+	clear_selection();
+	sys.selectionStart = selectionStart;
+	sys.selectionEnd = selectionEnd;
+
+	if (table.rows.length == 2) {
+		sys.c1 = selectionStart.cellIndex;
+		sys.c2 = selectionEnd.cellIndex;
+		return
+	} else if (table.rows[0].cells.length == 2) {
+		sys.r1 = selectionStart.parentNode.rowIndex;
+		sys.r2 = selectionEnd.parentNode.rowIndex;
+		return
+	}
+	var _ = normalize_selection(selectionStart, selectionEnd);
+	select(Math.max(_.r1, 2), Math.max(_.c1, 2), _.r2, _.c2);
+}
+
+function clear_selection() {
+	var rowCount = table.rows.length;
+	for (var rowNo = 0; rowNo < rowCount; rowNo++) {
+		var cells = table.rows[rowNo].cells;
+		var colCount = cells.length;
+		for (var colNo = 0; colNo < colCount; colNo++)
+			cells[colNo].classList.remove('cell_selected');
+	}
+	sys.r1 = null;
+	sys.c1 = null;
+	sys.r2 = null;
+	sys.c2 = null;
+}
+
+function reset_selection() {
+	clear_selection();
+	sys.selectionStart = sys.selectionEnd = null;
+}
+
+function normalize_selection(selectionStart, selectionEnd) {
+	var r1, c1, r2, c2;
+	if (selectionStart.classList.contains('cell')) {
+		// a simple cell was the start
+		r1 = selectionStart.parentNode.rowIndex;
+		c1 = selectionStart.cellIndex;
+		r2 = selectionEnd.parentNode.rowIndex;
+		c2 = selectionEnd.cellIndex;
+	} else if (selectionStart.classList.contains('colheader')) {
+		// a column header was the start
+		r1 = 2;
+		c1 = selectionStart.cellIndex;
+		r2 = table.rows.length - 1;
+		c2 = selectionEnd.cellIndex;
+	} else if (selectionStart.classList.contains('rowheader')) {
+		// a row header was the start
+		r1 = selectionStart.parentNode.rowIndex;
+		c1 = 2;
+		r2 = selectionEnd.parentNode.rowIndex;
+		c2 = table.rows[0].cells.length - 1;
+	}
+	return {
+		r1 : Math.min(r1, r2),
+		c1 : Math.min(c1, c2),
+		r2 : Math.max(r1, r2),
+		c2 : Math.max(c1, c2),
+	}
+}
+
+function mergeSelectedCells() {
+	for (var r = sys.r1; r <= sys.r2; r++) {
+		for (var c = sys.c1; c <= sys.c2; c++) {
+			if (r == sys.r1 && c == sys.c1)
+				continue;
+			var cell = table.rows[r].cells[c];
+			cell.classList.add('merged_cell');
+			cell.setAttribute('data-merged', [sys.r1, sys.c1].join(','));
+		}
+	}
+	var cell = table.rows[sys.r1].cells[sys.c1];
+	cell.colSpan = sys.c2 - sys.c1 + 1;
+	cell.rowSpan = sys.r2 - sys.r1 + 1;
+	sys.selectionHasMergedCells = true;
+}
+
+function splitSelectedCells() {
+	for (var r = sys.r1; r <= sys.r2; r++) {
+		for (var c = sys.c1; c <= sys.c2; c++) {
+			var cell = table.rows[r].cells[c];
+			cell.classList.remove('merged_cell');
+			cell.removeAttribute('data-merged');
+			cell.rowSpan = 1;
+			cell.colSpan = 1;
+		}
+	}
+	sys.selectionHasMergedCells = false;
+}
+
+function clearSelectionContents() {
+	for (var r = sys.r1; r <= sys.r2; r++) {
+		for (var c = sys.c1; c <= sys.c2; c++) {
+			var cell = table.rows[r].cells[c];
+			cell.innerHTML = '';
+		}
+	}
+}
+
+$.contextMenu({
+	selector : '#table > tbody > tr > td.cell_selected',
+	build : function($trigger, e) {
+		var items = {};
+		if (sys.selectionHasMergedCells)
+			items['split_cells'] = {
+				'name' : "Split merged cells",
+				'callback' : splitSelectedCells,
+				'icon' : 'split_cell',
+			};
+		else if (sys.selectionStart !== sys.selectionEnd)
+			items['mergeSelectedCells'] = {
+				'name' : "Merge selected cells",
+				'callback' : mergeSelectedCells,
+				'icon' : 'merge_cells',
+			};
+		items['clear_contents'] = {
+			'name' : "Clear contents",
+			'callback' : clearSelectionContents,
+			'icon' : 'clear_cell_contents',
+		};
+		items['formatSelectedCells'] = {
+			'name' : "Format...",
+			'callback' : formatSelection,
+		};
+
+		return {
+			'items' : items
+		}
+	}
+});
+
+$(document).on("mousedown", "#table > tbody > tr > td.colheader, #table > tbody > tr > td.rowheader", function(e) {
+	if (!e.shiftKey)
+		return;
+	sys.draggingStart = $(this);
+	sys.draggingStart.data("startX", e.pageX);
+	sys.draggingStart.data("startY", e.pageY);
+	sys.draggingStart.data("startWidth", $(this).width());
+	sys.draggingStart.data("startHeight", $(this).height());
+});
+
+$(document).on("mousemove", function(e) {
+	if (sys.draggingStart) {
+		if (sys.draggingStart.hasClass("colheader"))
+			sys.draggingStart.width(sys.draggingStart.data("startWidth") + (e.pageX - sys.draggingStart.data("startX")));
+		else
+			sys.draggingStart.height(sys.draggingStart.data("startHeight") + (e.pageY - sys.draggingStart.data("startY")));
+	}
+});
+
+$(document).on("mouseup", function() {
+	if (sys.draggingStart) {
+		sys.draggingStart = null;
+	}
 });
 
 function formatSelection() {
@@ -501,8 +412,108 @@ function removeColumns(colNo, count) {
 	count = count || 1;
 	var rowCount = table.rows.length;
 	for (var rowNo = 0; rowNo < rowCount; rowNo++) {
+		var row = table.rows[rowNo];
 		for (var i = 0; i < count; i++)
-			table.rows[rowNo].deleteCell(colNo);
+			row.deleteCell(colNo);
 	}
-	updateNumbering(0, 0, colNo, count);
+	updateNumbering(0, 0, colNo, -count);
 }
+
+function removeRows(rowNo, count) {
+	rowNo += 2;
+	count = count || 1;
+	for (var i = 0; i < count; i++)
+		table.deleteRow(rowNo);
+	updateNumbering(rowNo, -count);
+}
+
+$.contextMenu({
+	selector : '#table > tbody > tr > td.corner',
+	build : function($trigger, e) {
+		var el = $trigger[0];
+		return {
+			items : {
+				add_column : {
+					name : "Add a column",
+					callback : function(e) {
+						addColumns();
+					}
+				},
+				add_row : {
+					name : "Add a row",
+					callback : function(e) {
+						addRows();
+					}
+				},
+			}
+		}
+	}
+});
+
+$.contextMenu({
+	selector : '#table > tbody > tr > td.colheader',
+	build : function($trigger, e) {
+		var el = $trigger[0];
+		if (!sys.selectionStart)
+			set_selection(el, el);
+		else {
+			if (sys.selectionStart.classList.contains('colheader')) {
+				if (el.cellIndex < sys.c1 || el.cellIndex > sys.c2)
+					set_selection(el, el);
+			} else
+				set_selection(el, el);
+		}
+		return {
+			items : {
+				insert_columns : {
+					name : "Insert columns",
+					callback : function(e) {
+						addColumns(sys.c2 - sys.c1 + 1, sys.c1 - 2);
+						set_selection(sys.selectionStart, sys.selectionEnd);
+					}
+				},
+				remove_columns : {
+					name : "Remove columns",
+					callback : function(e) {
+						removeColumns(sys.c1 - 2, sys.c2 - sys.c1 + 1);
+						reset_selection();
+					}
+				},
+			}
+		}
+	}
+});
+
+$.contextMenu({
+	selector : '#table > tbody > tr > td.rowheader',
+	build : function($trigger, e) {
+		var el = $trigger[0];
+		if (!sys.selectionStart)
+			set_selection(el, el);
+		else {
+			if (sys.selectionStart.classList.contains('rowheader')) {
+				if (el.parentNode.rowIndex < sys.r1 || el.parentNode.rowIndex > sys.r2)
+					set_selection(el, el);
+			} else
+				set_selection(el, el);
+		}
+		return {
+			items : {
+				insert_rows : {
+					name : "Insert rows",
+					callback : function(e) {
+						addRows(sys.r2 - sys.r1 + 1, sys.r1 - 2);
+						set_selection(sys.selectionStart, sys.selectionEnd);
+					}
+				},
+				remove_rows : {
+					name : "Remove rows",
+					callback : function(e) {
+						removeRows(sys.r1 - 2, sys.r2 - sys.r1 + 1);
+						reset_selection();
+					}
+				},
+			}
+		}
+	}
+});
